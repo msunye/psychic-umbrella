808fdd4c51b14fa61eed917ca39754f7b8f7c55b
Marc Suñé <msunye@gmail.com>
2018-05-01 18:46:48 +0200

    ja funciona

--- boostmmap/boostmmap/boostmmap.cpp
+++ boostmmap/boostmmap/boostmmap.cpp
@@ -3,6 +3,9 @@
 
 #include "stdafx.h"
 
+#include <iostream>
+#include <boost/interprocess/managed_mapped_file.hpp>
+
 #include <boost/interprocess/file_mapping.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
@@ -18,8 +21,9 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	using namespace boost::interprocess;
 
-	file_mapping m_file("c:\\users\\marc\\mmfile", read_write);
-	mapped_region region(m_file, read_write, 0, 0 );
+	managed_mapped_file mfile(open_or_create
+		, "c:\\users\\marc\\mmfile" //Mapped file name
+		, 65536);        //Mapped file size
 
 	//Note that unordered_map<Key, MappedType>'s value_type is std::pair<const Key, MappedType>,
 	//so the allocator must allocate that pair.
@@ -28,27 +32,33 @@ int _tmain(int argc, _TCHAR* argv[])
 	typedef std::pair<const int, float> ValueType;
 
 	//Typedef the allocator
-	typedef allocator<ValueType, file_mapping::segment_manager> ShmemAllocator;
+	typedef allocator<ValueType, managed_mapped_file::segment_manager> MapAllocator;
+	MapAllocator mapAllocator(mfile.get_segment_manager());
 
 	//Alias an unordered_map of ints that uses the previous STL-like allocator.
 	typedef boost::unordered_map
 		< KeyType, MappedType
 		, boost::hash<KeyType>, std::equal_to<KeyType>
-		, ShmemAllocator>
+		, MapAllocator>
 		MyHashMap;
 
 	//Construct a shared memory hash map.
 	//Note that the first parameter is the initial bucket count and
 	//after that, the hash function, the equality function and the allocator
-	MyHashMap *myhashmap = segment.construct<MyHashMap>("MyHashMap")  //object name
+	MyHashMap *myhashmap = mfile.find_or_construct<MyHashMap>("MyHashMap")  //object name
 		(3, boost::hash<int>(), std::equal_to<int>()                  //
-			, segment.get_allocator<ValueType>());                         //allocator instance
+			, mapAllocator);                         //allocator instance
 
-																		   //Insert data in the hash map
+	std::cout << "n:" << myhashmap->size();
+
+	for (int i = 0; i < 100; ++i) {
+		myhashmap->operator[](i+100) = (float)i * 2;
+	}
+
+	//Insert data in the hash map
 	for (int i = 0; i < 100; ++i) {
 		myhashmap->insert(ValueType(i, (float)i));
 	}
-	return 0;
 	return 0;
 }
 
